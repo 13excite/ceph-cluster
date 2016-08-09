41bd51297d3f8c6b905b9f27b3260ee74a009977
vladimir <vladimir@MacBook-Pro-vladimir.local>
2016-08-09 07:09:20 +0300

    fix

--- add_hosts.sh
+++ add_hosts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo '192.168.57.151 node00
-192.168.57.150 node01
-192.168.57.158 node02
-192.168.57.159 node-mon
-192.168.57.161 node-adm' >> /etc/hosts
+echo '192.168.57.151 ceph-node01
+192.168.57.150 ceph-node02
+192.168.57.158 ceph-node03
+192.168.57.159 ceph-mon
+192.168.57.161 ceph-adm' >> /etc/hosts
 
 cat /etc/hosts
